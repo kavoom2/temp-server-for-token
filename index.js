1a66f72830fc75f2ef173b2f9b22f40f9196a7d1
kavoom2 <kavoom2@gmail.com>
2021-03-21 19:30:08 +0900

    edit host to HTTPS

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const data = require("./data.js");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "OPTIONS"],
   })
